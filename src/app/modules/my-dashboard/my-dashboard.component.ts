fde6ddeea70cacdbb5bff2f0db3ab9ad9734e28c
ravisah33 <ravilive33@gmail.com>
2021-09-16 20:50:23 +0530

    4 files added

---
+++ src/app/modules/my-dashboard/my-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-dashboard',
+  templateUrl: './my-dashboard.component.html',
+  styleUrls: ['./my-dashboard.component.scss'],
+})
+export class MyDashboardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
