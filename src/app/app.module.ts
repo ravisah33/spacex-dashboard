41354b53089e223b35343acf5b24f0221bdc60d8
ravisah33 <ravilive33@gmail.com>
2021-09-17 21:05:37 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
+//import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule,
     FormsModule,
-    SharedModule,
+    //SharedModule,
     ReactiveFormsModule,
     AppRoutingModule,
   ],
